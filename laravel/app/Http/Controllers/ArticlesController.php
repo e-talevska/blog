a9cc4b252ae45f940184e24c500735679e73ce20
Jordan Jordanovski <jordanovskijordan@hotmail.com>
2016-06-08 20:59:42 +0200

    working with migration and models

---
+++ laravel/app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class ArticlesController extends Controller
+{
+    public function index(){
+        $articles = Article::all();
+        //["articles" => $articles]
+        return view("articles.list", compact("articles"));
+    }
+    public function view($slug){
+           $article = Article::where("slug", "=", $slug)->first();
+
+            if(is_null($article)){
+                abort('404');
+            }
+                return view('articles.view', compact('article'));
+        }
+
+}
---
+++ laravel/database/migrations/2016_06_08_164726_alter_articles.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterArticles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+           $table->string("featured_image");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropColumn("featured_image");
+        });
+    }
+}
