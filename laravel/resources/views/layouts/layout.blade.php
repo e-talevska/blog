de8ba74d62bcdb4d0b4f68de266fad4f8578250a
Emilija Pereska <emilija_pereska@hotmail.com>
2016-06-08 20:59:39 +0200

    Working with migrations and models

--- laravel/resources/views/layouts/layout.blade.php
+++ laravel/resources/views/layouts/layout.blade.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="content">
+    <div class="container">
         @yield('content')
     </div>
 </body>
