fd7000ebfaffce465b5bf8a9b6e061fc2d64aaad
Jordan Jordanovski <jordanovskijordan@hotmail.com>
2016-06-13 21:27:40 +0200

    laravel datepickertime, html

--- laravel/app/Http/Controllers/ArticlesController.php
+++ laravel/app/Http/Controllers/ArticlesController.php
@@ -22,5 +22,14 @@ class ArticlesController extends Controller
             }
                 return view('articles.view', compact('article'));
         }
+    public function create(){
+        return view("articles.create");
+    }
+    public function store(Request $request){
 
+        $input = $request->all();
+        $article = new Article();
+        $article->create($input);
+        return redirect('/articles');
+    }
 }
